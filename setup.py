b87752c56522caf3420379c962b2b79315ee7235
Arthur Taylor <arthur@advancedtelematic.com>
2017-05-06 13:31:17 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'arduino_lights',
   packages = ['arduino_lights'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'Control LED lights via Arduino',
   author = 'Arthur Taylor',
   author_email = 'arthur.taylor@gmail.com',
   url = 'https://github.com/craftcodiness/arduino-lights', # use the URL to the github repo
-  download_url = 'https://github.com/craftcodiness/arduino-lights/archive/0.2.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/craftcodiness/arduino-lights/archive/0.3.tar.gz', # I'll explain this in a second
   keywords = ['led', 'serial', 'arduino'],
   classifiers = [],
 )
