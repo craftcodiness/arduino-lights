959d38c406bf7c4dd15e29785d203016ef570b0b
Arthur Taylor <arthur@advancedtelematic.com>
2017-05-26 22:11:30 +0200

    Added gamma correction to correct for LED gamut.

--- arduino_lights/controller.py
+++ arduino_lights/controller.py
@@ -10,6 +10,28 @@ LED_COUNT = LED_SIZE.w * LED_SIZE.h
 BAUD_RATE = 115200
 
 
+#The WS2801 has a nonlinear brightness curve, so we need to compensate
+# to get a proper colour mapping.
+# @todo - this gamma correction should really be run on the Arduino.
+gammaCorrection = [
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 ]
+
+
 def connect(file="/dev/ttyUSB0"):
     file = os.getenv("BLEMU_DEVICE", file)
     if not os.path.exists(file):
@@ -34,13 +56,14 @@ def set_pixel(ser, coord, red, green, blue):
     If coord is a tuple, it will be interpreted as x, y coordinates.
     If it is an integer it's interpreted as a linear pixel address.
     '''
-    red = min(red, 253)
-    green = min(green, 253)
-    blue = min(blue, 253)
+    red = min(red, 254)
+    green = min(green, 254)
+    blue = min(blue, 254)
     pixel = xy_to_pixel(*coord) if isinstance(coord, tuple) else coord
-    if pixel > 253:
+    if pixel > 254:
         raise ValueError('Pixel number out of range')
-    control_string = bytearray([pixel, red, green, blue, 255])
+    control_string = bytearray([pixel, gammaCorrection[red], gammaCorrection[green], gammaCorrection[blue], 255])
+    #control_string = bytearray([pixel, red, green, blue, 255])
     ser.write(control_string)
     ser.flush()
 
